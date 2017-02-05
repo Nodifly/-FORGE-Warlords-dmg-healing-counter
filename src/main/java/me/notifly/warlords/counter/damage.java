f559e7beaa569cf5ce696e6651f54724602e120e
Dan <danbenison@gmail.com>
2017-02-05 13:22:53 +0000

    Added "totalDamage += damage" to make it look nicer?

--- src/main/java/me/notifly/warlords/counter/damage.java
+++ src/main/java/me/notifly/warlords/counter/damage.java
@@ -37,25 +37,25 @@ public class damage{
                     if (Character.isDigit(endOfMsg.charAt(0))) {
                         damageDigit = 1000; // damage > 1000
                         damage = Integer.parseInt(endOfMsg.substring(0, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
 
                     if (Character.isWhitespace(endOfMsg.charAt(0))) {
                         damageDigit = 100; // damage >= 100, damage < 1000
                         damage = Integer.parseInt(endOfMsg.substring(1, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
 
                     if (endOfMsg.startsWith("r")) {
                         damageDigit = 10; // damage >= 10, damage < 100
                         damage = Integer.parseInt(endOfMsg.substring(2, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
 
                     if (endOfMsg.startsWith("o")) {
                         damageDigit = 1; // damage < 10
                         damage = Integer.parseInt(endOfMsg.substring(3, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
                 }
 
@@ -65,13 +65,13 @@ public class damage{
                     if (Character.isDigit(endOfMsg.charAt(0))) {
                         damageDigit = 1000; // damage => 1000
                         damage = Integer.parseInt(endOfMsg.substring(0, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
 
                     if (Character.isWhitespace(endOfMsg.charAt(0))) {
                         damageDigit = 100; // damage >= 100, damage < 1000
                         damage = Integer.parseInt(endOfMsg.substring(1, 4));
-                        totalDamage = totalDamage + damage;
+                        totalDamage += damage;
                     }
                 }
             }
